8595a7d62acb6c3b221c941994d13f10d84c38e2
Mr-Robot-101 <git.sujon@gmail.com>
2022-10-31 14:12:51 +0600

    Header Ready

--- src/Pages/Share/Header/Header.js
+++ src/Pages/Share/Header/Header.js
@@ -9,20 +9,7 @@ const Header = () => {
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
+            
             </label>
             <ul
               tabIndex={0}
@@ -49,7 +36,7 @@ const Header = () => {
               </li>
             </ul>
           </div>
-          <Link className="btn btn-ghost normal-case text-xl"><img className="w-16" src={logo} alt="" /></Link>
+          <Link to='/' className="btn btn-ghost normal-case text-xl"><img className="w-16" src={logo} alt="" /></Link>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal p-0">
